a02883877d30aae00792e976b7e881b5da3e3314
zxxng <jeayeong123@naver.com>
2024-03-05 19:08:53 +0900

    feat(travelClub): Collection을 활용한 interface 생성

--- travel-club/step01-TravelClub/src/io/namoosori/java/travelClub/store/logic/ClubMapStore.java
+++ travel-club/step01-TravelClub/src/io/namoosori/java/travelClub/store/logic/ClubMapStore.java
@@ -3,37 +3,59 @@ package io.namoosori.java.travelClub.store.logic;
 import io.namoosori.java.travelClub.entity.TravelClub;
 import io.namoosori.java.travelClub.store.ClubStore;
 
-import java.util.List;
+import java.util.*;
 
 public class ClubMapStore implements ClubStore {
+    private Map<String, TravelClub> clubMap;
+
+    public ClubMapStore() {
+        this.clubMap = new HashMap<>();
+    }
 
     @Override
     public void create(TravelClub travelClub) {
-
+        this.clubMap.put(travelClub.getId(), travelClub);
     }
 
     @Override
     public TravelClub retrieve(String clubId) {
-        return null;
+        return this.clubMap.get(clubId);
     }
 
     @Override
     public List<TravelClub> retrieveAllByName(String clubName) {
-        return null;
+        List<TravelClub> foundClubs = new ArrayList<>();
+
+        for (TravelClub club: this.clubMap.values()) {
+            if (club.getClubName().equals(clubName)) {
+                foundClubs.add(club);
+            }
+        }
+
+        // iterator를 활용한 방법
+//        Iterator<TravelClub> iterator = this.clubMap.values().iterator();
+//        while (iterator.hasNext()){
+//            TravelClub club = iterator.next();
+//            if (club.getClubName().equals(clubName)) {
+//                foundClubs.add(club);
+//            }
+//        }
+
+        return foundClubs;
     }
 
     @Override
     public List<TravelClub> retrieveAll() {
-        return null;
+        return new ArrayList<>(this.clubMap.values());
     }
 
     @Override
     public void update(TravelClub updateClub) {
-
+        this.clubMap.put(updateClub.getId(), updateClub);
     }
 
     @Override
     public void delete(String clubId) {
-
+        this.clubMap.remove(clubId);
     }
 }
