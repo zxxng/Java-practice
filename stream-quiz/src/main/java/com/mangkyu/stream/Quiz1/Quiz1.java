1ad336416ddd24fee74d6399e95eda358763019c
zxxng <jeayeong123@naver.com>
2024-03-26 13:31:57 +0900

    add(streamQuiz): stream 연습용 폴더 추가

---
+++ stream-quiz/src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
@@ -0,0 +1,36 @@
+package com.mangkyu.stream.Quiz1;
+
+import com.opencsv.CSVReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Quiz1 {
+
+    // 1.1 각 취미를 선호하는 인원이 몇 명인지 계산하여라.
+    public Map<String, Integer> quiz1() throws IOException {
+        List<String[]> csvLines = readCsvLines();
+        return new HashMap<>();
+    }
+
+    // 1.2 각 취미를 선호하는 정씨 성을 갖는 인원이 몇 명인지 계산하여라.
+    public Map<String, Integer> quiz2() throws IOException {
+        List<String[]> csvLines = readCsvLines();
+        return new HashMap<>();
+    }
+
+    // 1.3 소개 내용에 '좋아'가 몇번 등장하는지 계산하여라.
+    public int quiz3() throws IOException {
+        List<String[]> csvLines = readCsvLines();
+        return 0;
+    }
+
+    private List<String[]> readCsvLines() throws IOException {
+        CSVReader csvReader = new CSVReader(new FileReader(getClass().getResource("/user.csv").getFile()));
+        csvReader.readNext();
+        return csvReader.readAll();
+    }
+
+}
