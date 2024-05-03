1f94eef6fc44c595a9036a19796356104b113d14
zxxng <jeayeong123@naver.com>
2024-05-03 09:35:46 +0900

    feat(spring-basic): baen 조회

--- spring-basic/src/main/java/com/example/demo/MemberApp.java
+++ spring-basic/src/main/java/com/example/demo/MemberApp.java
@@ -13,6 +13,7 @@ public class MemberApp {
 //        MemberService memberService = appConfig.memberService();
 
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
         MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);
--- spring-basic/src/main/java/com/example/demo/OrderApp.java
+++ spring-basic/src/main/java/com/example/demo/OrderApp.java
@@ -16,6 +16,7 @@ public class OrderApp {
 //        MemberService memberService = appConfig.memberService();
 //        OrderService orderService = appConfig.orderService();
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
         MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
         OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
